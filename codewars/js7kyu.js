328a562766dc704092c0a92c4b3a20242fc4ba53
Павел <tepek92@mail.ru>
2022-06-18 13:38:30 +0700

    codewars 7kyu kata

--- codewars/js7kyu.js
+++ codewars/js7kyu.js
@@ -43,3 +43,55 @@ function alternate(n, firstValue, secondValue){
         }
         return result.slice(0, -1);
       }
+
+
+// Get the Middle Character
+/*
+You are going to be given a word. 
+Your job is to return the middle character of the word. 
+If the word's length is odd, return the middle character. 
+If the word's length is even, return the middle 2 characters.
+
+#Examples:
+Kata.getMiddle("test") should return "es"
+Kata.getMiddle("testing") should return "t"
+Kata.getMiddle("A") should return "A"
+*/
+      const getMiddle = s => (s.length % 2 == 0) ? s.substr(s.length / 2 - 1, 2) : s[Math.floor(s.length / 2)];
+
+
+// Rock Paper Scissors Lizard Spock
+      function rpsls(pl1,pl2){
+        let result = "";
+        if (pl1 === "paper" && (pl2 === "rock" || pl2 === "spock")) {
+          result = "Player 1 Won!";
+        } else if (pl1 === "scissors" && (pl2 === "paper" || pl2 === "lizard")) {
+          result = "Player 1 Won!";
+        } else if (pl1 === "rock" && (pl2 === "lizard" || pl2 === "scissors")) {
+          result = "Player 1 Won!";
+        } else if (pl1 === "lizard" && (pl2 === "spock" || pl2 === "paper")) {
+          result = "Player 1 Won!";
+        } else if (pl1 === "spock" && (pl2 === "scissors" || pl2 === "rock")) {
+          result = "Player 1 Won!";
+        } else if (pl1 === pl2) {
+          result = "Draw!";
+        } else {
+           result = "Player 2 Won!";
+         } 
+        return result;
+      }
+
+      function rpsls(pl1,pl2){
+        const winner = {
+          scissors: ["paper", "lizard"],
+          paper: ["rock", "spock"],
+          rock: ["lizard", "scissors"],
+          lizard: ["spock", "paper"],
+          spock: ["scissors", "rock"]
+        };
+        
+        if (pl1 == pl2) {
+          return "Draw!";
+        }
+        return winner[pl1].includes(pl2) ? "Player 1 Won!" : "Player 2 Won!";
+      }
