dbf13d75ccecb3e335b1f6ade83fc870751b8721
Павел <tepek92@mail.ru>
2022-06-19 12:52:39 +0700

    codewars 7kyu kata

--- codewars/js7kyu.js
+++ codewars/js7kyu.js
@@ -95,3 +95,51 @@ Kata.getMiddle("A") should return "A"
         }
         return winner[pl1].includes(pl2) ? "Player 1 Won!" : "Player 2 Won!";
       }
+
+// Maximum Length Difference
+// You are given two arrays a1 and a2 of strings. 
+// Each string is composed with letters from a to z. 
+// Let x be any string in the first array
+// and y be any string in the second array.
+// Find max(abs(length(x) − length(y)))
+// a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+// a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+// mxdiflg(a1, a2) --> 13
+
+
+function mxdiflg(a1, a2) {
+  let result = -1;
+  let temp;
+    for (let i = 0; i < a1.length; i++) {
+      for (let j = 0; j < a2.length; j++) {
+        temp = Math.abs(a1[i].length - a2[j].length);
+        result = temp > result ? temp : result;
+      }
+    }
+  return result;
+}
+// не мое решение
+function mxdiflg(a1, a2) {
+  if (a1.length === 0 || a2.length === 0) return -1
+  let l1 = a1.map(str => str.length)
+  let l2 = a2.map(str => str.length)
+  return Math.max(Math.max(...l1) - Math.min(...l2), Math.max(...l2) - Math.min(...l1))
+}
+
+// sPoNgEbOb MeMe
+// input:  "stop Making spongebob Memes!"
+// output: "StOp mAkInG SpOnGeBoB MeMeS!"
+function spongeMeme(sentence) {
+  let result = "";
+  for (let i = 0; i < sentence.length; i++) {
+    result += (i + 1) % 2 == 0 ? sentence[i].toLowerCase() : sentence[i].toUpperCase();
+  }
+  return result;
+}
+
+let spongeMeme = sentence => 
+        Array.from(sentence).reduce((str, word) => 
+                str += ((str.length + 1) % 2 == 0) ? 
+                word.toLowerCase() : word.toUpperCase(), "");
+
+
