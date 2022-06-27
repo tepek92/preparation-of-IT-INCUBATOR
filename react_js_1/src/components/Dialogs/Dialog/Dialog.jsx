89a0d3908bb4d63b957be081c3ffc07b77e79a50
Павел <tepek92@mail.ru>
2022-06-27 22:10:59 +0700

    29: friens to NavBar

--- react_js_1/src/components/Dialogs/Dialog/Dialog.jsx
+++ react_js_1/src/components/Dialogs/Dialog/Dialog.jsx
@@ -4,7 +4,6 @@ import s from "./Dialog.module.css";
 const Dialog = (props) => {
   return (
       <div className={s.dialog}>
-        
         <NavLink to={"/dialogs/" + props.id} className={(a) => a.isActive ? s.active : ""}>
           <img src={props.avatar} />
           {props.name}
--- react_js_1/src/components/Navbar/Navbar.jsx
+++ react_js_1/src/components/Navbar/Navbar.jsx
@@ -4,11 +4,21 @@ import LinkBar from "./Linkbar/Linkbar";
 const Navbar = () => {
   return (
     <nav className={s.nav}>
-      <LinkBar path="/profile" text="Profile" />
-      <LinkBar path="/dialogs" text="Messages" />
-      <LinkBar path="/news" text="News" />
-      <LinkBar path="/music" text="Music" />
-      <LinkBar path="/settings" text="Settings" />
+      <div className={s.item}>
+        <LinkBar path="/profile" text="Profile" />
+        <LinkBar path="/dialogs" text="Messages" />
+        <LinkBar path="/news" text="News" />
+        <LinkBar path="/music" text="Music" />
+      </div>
+      <div className={s.setting}>
+        <LinkBar path="/settings" text="Settings" />
+      </div>
+      <div className={s.friends}>
+      <LinkBar path="/friends" text="Friends" />
+      <img src="https://shutnikov.club/wp-content/uploads/2020/01/1_PiHoomzwh9Plr9_GA26JcA.png" />
+      <img src="https://shutnikov.club/wp-content/uploads/2020/01/mschropp_avatar-300x300.png" />
+      <img src="https://i1.photo.2gis.com/images/profile/30258560049350742_9a4a.jpg" />
+      </div>
     </nav>
   );
 };
