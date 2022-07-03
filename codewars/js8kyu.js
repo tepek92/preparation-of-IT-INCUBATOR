f926f50eb4f9b7f9e3cd39c800e78c1ad42b82b5
Павел <tepek92@mail.ru>
2022-07-04 00:34:18 +0700

    kata 8 kyu

--- codewars/js8kyu.js
+++ codewars/js8kyu.js
@@ -2,15 +2,15 @@
 
 // Sum Numbers
 // Сложить все числа в массиве
-function sum (numbers) {
-    let result = 0.0;
-    if (numbers[0]) {
-      for (let i = 0; i < numbers.length; i++) {
-        result += numbers[i];
-      }
+function sum(numbers) {
+  let result = 0.0;
+  if (numbers[0]) {
+    for (let i = 0; i < numbers.length; i++) {
+      result += numbers[i];
     }
-      return result;
-  };
+  }
+  return result;
+}
 
 // Return a greeting string
 // Вернуть строку с входящим именем.
@@ -26,14 +26,14 @@ Example:
 ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
 */
 function removeEveryOther(arr) {
-    let newArr = [];
-    for (let i = 0; i < arr.length; i++) {
-      if (i % 2 == 0) {
-        newArr.push(arr[i]);
-      }
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 == 0) {
+      newArr.push(arr[i]);
     }
-    return newArr;
   }
+  return newArr;
+}
 
 // Count of positives / sum of negatives
 /*
@@ -46,25 +46,25 @@ For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15],
 you should return [10, -65].
 */
 function countPositivesSumNegatives(input) {
-    let resultArray = [];
-    //console.log(input.length);
-    if (Array.isArray(input)) {
-      if (input.length > 0) {
-        let countPositive = 0;
-        let sumNegative = 0;
-        for (let i = 0; i < input.length; i++) {
-          if (input[i] > 0) {
-            countPositive++;
-          } else if (input[i] < 0) {
-            sumNegative += input[i];
-          }
+  let resultArray = [];
+  //console.log(input.length);
+  if (Array.isArray(input)) {
+    if (input.length > 0) {
+      let countPositive = 0;
+      let sumNegative = 0;
+      for (let i = 0; i < input.length; i++) {
+        if (input[i] > 0) {
+          countPositive++;
+        } else if (input[i] < 0) {
+          sumNegative += input[i];
         }
-        resultArray.push(countPositive);
-        resultArray.push(sumNegative);
       }
+      resultArray.push(countPositive);
+      resultArray.push(sumNegative);
     }
-      return resultArray;
   }
+  return resultArray;
+}
 
 // Beginner - Reduce but Grow
 /* 
@@ -74,25 +74,25 @@ Example:
 [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 */
 function grow(x) {
-    return x.reduce((mult, n) => mult *= n, 1);
-  }
+  return x.reduce((mult, n) => (mult *= n), 1);
+}
 
 // Abbreviate a Two Word Name
 /*
 Sam Harris => S.H
 patrick feeney => P.F
   */
-function abbrevName(name){
-    let nameArray = name.split(" ");
-    let firstName = nameArray[0];
-    let secondName = nameArray[1];
-    return firstName[0].toUpperCase() + "." + secondName[0].toUpperCase();
-  }
+function abbrevName(name) {
+  let nameArray = name.split(" ");
+  let firstName = nameArray[0];
+  let secondName = nameArray[1];
+  return firstName[0].toUpperCase() + "." + secondName[0].toUpperCase();
+}
 
 // Convert a string to an array
-function stringToArray(string){
-    return string.split(" ");
-  }
+function stringToArray(string) {
+  return string.split(" ");
+}
 
 // Count by X
 /*
@@ -104,14 +104,14 @@ countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
 countBy(2,5) === [2,4,6,8,10]
 */
 function countBy(x, n) {
-    let result = [];
-    for (let i = x; result.length != n; i++) {
-      if (i % x == 0) {
-        result.push(i);
-      }
+  let result = [];
+  for (let i = x; result.length != n; i++) {
+    if (i % x == 0) {
+      result.push(i);
     }
-    return result;
   }
+  return result;
+}
 // Find the first non-consecutive number
 /*
 Your task is to find the first element of an array that is not consecutive.
@@ -121,24 +121,31 @@ then 1 then 2 then 3 then 4 are all consecutive but 6 is not,
 so that's the first non-consecutive number.
 If the whole array is consecutive then return null2.
 */
-function firstNonConsecutive (arr) {
-    for (let i = 1; i < arr.length; i++) {
-      if (arr [i] != arr[i - 1] + 1) {
-        return arr[i];
-      }
+function firstNonConsecutive(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] != arr[i - 1] + 1) {
+      return arr[i];
     }
-    return null;
   }
+  return null;
+}
 
-  // I love you, a little , a lot, passionately ... not at all НЕ ПРАВИЛЬНО
-  // Любит не любит
-  function howMuchILoveYou(nbPetals) {
-    let array = ["I love you", "a little", "a lot", "passionately", "madly", "not at all"];
-    return nbPetals <= 6 ? array[nbPetals - 1] : array[nbPetals - 7];
-  }
+// I love you, a little , a lot, passionately ... not at all НЕ ПРАВИЛЬНО
+// Любит не любит
+function howMuchILoveYou(nbPetals) {
+  let array = [
+    "I love you",
+    "a little",
+    "a lot",
+    "passionately",
+    "madly",
+    "not at all",
+  ];
+  return nbPetals <= 6 ? array[nbPetals - 1] : array[nbPetals - 7];
+}
 
-  // 7kyu Even numbers in an array
-  /*
+// 7kyu Even numbers in an array
+/*
 Given an array of numbers, return a new array of length number containing
  the last even numbers from the original array (in the same order).
  The original array will be not empty and will contain at least "number" even numbers.
@@ -148,41 +155,41 @@ For example:
 ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
   */
 function evenNumbers(array, number) {
-    let temp = [];
-    let result = [];
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] % 2 == 0) {
-        temp.push(array[i]);
-      }
+  let temp = [];
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 == 0) {
+      temp.push(array[i]);
     }
-    for (let j = 0; j < number; j++) {
-      result.push(temp.pop());
-    }
-    return result.reverse();
   }
-
-  // Count Odd Numbers below n
-  let oddCount = n => Math.floor(n / 2);
-
-  // Эти алгоритмы не проходили тест по времени
-  function oddCount(n) {
-    let count = 0;
-    for (let i = 0; i < n; i++) {
-      if (i % 2 != 0) count++;
-    }
-    return count;
+  for (let j = 0; j < number; j++) {
+    result.push(temp.pop());
   }
-  
-  let oddCount = n => { 
-    let count = 0;
-    for (let i = 0; i < n; i++) {
-      if (i % 2 != 0) count++;
-    }
-    return count;
-  };
+  return result.reverse();
+}
 
-  // Enumerable Magic #20 - Cascading Subsets
- /*
+// Count Odd Numbers below n
+let oddCount = (n) => Math.floor(n / 2);
+
+// Эти алгоритмы не проходили тест по времени
+function oddCount(n) {
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    if (i % 2 != 0) count++;
+  }
+  return count;
+}
+
+let oddCount = (n) => {
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    if (i % 2 != 0) count++;
+  }
+  return count;
+};
+
+// Enumerable Magic #20 - Cascading Subsets
+/*
 Create a method each_cons that accepts a list and a number n,
 and returns cascading subsets of the list of size n, like so:
 each_cons([1,2,3,4], 2)
@@ -190,35 +197,34 @@ each_cons([1,2,3,4], 2)
 each_cons([1,2,3,4], 3)
   #=> [[1,2,3],[2,3,4]]
  */
-  function eachCons(array, n) {
-    let arrResult = [];
-    let arrTemp = [];
-    for (let i = 0; i <  array.length; i++) {
-      for (let j = 0; j < n; j++) {
-        if (array[i + j] != null) {
-          arrTemp.push(array[i + j]);
-        }
+function eachCons(array, n) {
+  let arrResult = [];
+  let arrTemp = [];
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < n; j++) {
+      if (array[i + j] != null) {
+        arrTemp.push(array[i + j]);
       }
-      if(arrTemp.length == n) {
-        arrResult.push(arrTemp);
-      }
-      arrTemp = [];
     }
-    return arrResult;
-  }
-
-  // Версия с .slise and .filter
-  function eachCons(array, n) {
-    let resultArr = [];
-    for (let i = 0; i < array.length; i++) {
-      resultArr.push(array.slice(i, i + n));
-      
+    if (arrTemp.length == n) {
+      arrResult.push(arrTemp);
     }
-    return resultArr.filter((resultArr) => resultArr.length == n);
+    arrTemp = [];
   }
+  return arrResult;
+}
 
-  //Opposites Attract
-  /*
+// Версия с .slise and .filter
+function eachCons(array, n) {
+  let resultArr = [];
+  for (let i = 0; i < array.length; i++) {
+    resultArr.push(array.slice(i, i + n));
+  }
+  return resultArr.filter((resultArr) => resultArr.length == n);
+}
+
+//Opposites Attract
+/*
 Timmy & Sarah think they are in love, 
 but around where they live,
 they will only know once they pick a flower each.
@@ -230,22 +236,24 @@ if they are in love and false if they aren't.
   */
 
 let lovefunc = (flower1, flower2) => {
-  return ((flower1 % 2 == 0 && flower2 % 2 != 0)
-     || (flower1 % 2 != 0 && flower2 % 2 == 0));
+  return (
+    (flower1 % 2 == 0 && flower2 % 2 != 0) ||
+    (flower1 % 2 != 0 && flower2 % 2 == 0)
+  );
 };
 
-let lovefunc = (flower1, flower2) => !!((flower1 + flower2) % 2); 
+let lovefunc = (flower1, flower2) => !!((flower1 + flower2) % 2);
 //равно
-let lovefunc = (flower1, flower2) => Boolean(((flower1 + flower2) % 2));
+let lovefunc = (flower1, flower2) => Boolean((flower1 + flower2) % 2);
 // т.е. !!(двойное отрицание) равно преобразованиею в Boolean(value)
 
-let lovefunc = (flower1, flower2) => ((flower1 + flower2) % 2 != 0);
-
+let lovefunc = (flower1, flower2) => (flower1 + flower2) % 2 != 0;
 
 //Will you make it?
 // длинное "сложное" услови, по факту
 // просто нужно просчитать хватит ли бензина на дистанцию при заданном расходе
-const zeroFuel = (distanceToPump, mpg, fuelLeft) => distanceToPump <= mpg * fuelLeft;
+const zeroFuel = (distanceToPump, mpg, fuelLeft) =>
+  distanceToPump <= mpg * fuelLeft;
 
 //Exclusive "or" (xor) Logical Operator
 // реализовать "исключающее или"
@@ -257,3 +265,79 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => distanceToPump <= mpg * fuel
 let xor = (a, b) => !!(+a != +b);
 let xor = (a, b) => a != b;
 
+// L1: Set Alarm
+// setAlarm(true, true) -> false
+// setAlarm(false, true) -> false
+// setAlarm(false, false) -> false
+// setAlarm(true, false) -> true
+
+const setAlarm = (employed, vacation) => employed && !vacation;
+
+// Total amount of points
+// Our football team finished the championship. The result of each match look like "x:y".
+// Results of all matches are recorded in the collection.
+// For example: ["3:1", "2:2", "0:1", ...]
+// Write a function that takes such collection and counts the
+// points of our team in the championship.
+// Rules for counting points for each match:
+// if x > y: 3 points
+// if x < y: 0 point
+// if x = y: 1 point
+
+// 1
+function points(games) {
+  games = games.map((score) => score.split(":"));
+
+  let sum = games.reduce((sum, arr) => {
+    if (arr[0] > arr[1]) {
+      sum += 3;
+    } else if (arr[0] == arr[1]) {
+      sum++;
+    }
+    return sum;
+  }, 0);
+  return sum;
+}
+
+// 2
+const points = (games) =>
+  games
+    .map((score) => score.split(":"))
+    .reduce(
+      (sum, point) =>
+        point[0] > point[1] ? (sum += 3) : point[0] == point[1] ? ++sum : sum,
+      0
+    );
+
+// A wolf in sheep's clothing
+// Examples
+// Input: ["sheep", "sheep", "sheep", "wolf", "sheep"]
+// Output: "Oi! Sheep number 1! You are about to be eaten by a wolf!"
+
+// Input: ["sheep", "sheep", "wolf"]
+// Output: "Pls go away and stop eating my sheep"
+
+//1
+function warnTheSheep(queue) {
+  for (let i = 0; i < queue.length - 1; i++) {
+    if (queue[i] === "wolf") {
+      return (
+        "Oi! Sheep number " +
+        (queue.length - i - 1) +
+        "! You are about to be eaten by a wolf!"
+      );
+    }
+  }
+  return "Pls go away and stop eating my sheep";
+}
+
+//2
+const warnTheSheep = (queue) =>
+  queue.reverse().indexOf("wolf") == 0
+    ? "Pls go away and stop eating my sheep"
+    : `Oi! Sheep number ${queue.indexOf(
+        "wolf"
+      )}! You are about to be eaten by a wolf!`;
+
+//Convert a String to a Number!
+const stringToNumber = (str) => +str;
