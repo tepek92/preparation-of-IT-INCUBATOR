ca11a27469cf5edf14c8eca062ddc3db7d9f03fa
Павел <tepek92@mail.ru>
2022-07-05 20:15:07 +0700

    54: componentDidMount

--- react_js_1/src/components/Users/Users.jsx
+++ react_js_1/src/components/Users/Users.jsx
@@ -1,16 +1,13 @@
 import axios from "axios";
 import React from "react";
 import s from "./Users.module.css";
-import userPhoto from "../../img/user.png"
+import userPhoto from "../../img/user.png";
 
 class Users extends React.Component {
-  constructor(props) {
-    super(props);
-
+  componentDidMount() {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
       .then((response) => this.props.setUsers(response.data.items));
-
   }
 
   render() {
@@ -19,7 +16,11 @@ class Users extends React.Component {
         {this.props.users.map((u) => {
           return (
             <div>
-              <img className={s.userAvatar} src={u.photos.small || userPhoto} alt="" />
+              <img
+                className={s.userAvatar}
+                src={u.photos.small || userPhoto}
+                alt=""
+              />
               <div> {u.name} </div>
               <div> {u.status} </div>
               <button onClick={() => this.props.follow(u.id)}>
