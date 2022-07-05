36d7bb0e571ef1fe5b6739b54c325bb9dea1e1f0
Павел <tepek92@mail.ru>
2022-07-05 18:11:10 +0700

    51: user API, get users

--- react_js_1/src/components/Profile/MyPosts/Post/Post.jsx
+++ react_js_1/src/components/Profile/MyPosts/Post/Post.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import s from "./Post.module.css";
 
 const Post = (props) => {
-  debugger
   return (
     <div className={s.item}>
       <img
--- react_js_1/src/redux/DELstore.js
+++ react_js_1/src/redux/DELstore.js
@@ -83,3 +83,32 @@ let store = {
 };
 
 export default store;
+
+
+
+// [
+//   {
+//     id: 1,
+//     photo: "https://avatarko.ru/img/kartinka/12/multfilm_rusalka_Ariel_11649.jpg",
+//     followed: true,
+//     fullName: "Maria Savina",
+//     status: "Im HAPPY!",
+//     adress: { country: "France", city: "Paris" },
+//   },
+//   {
+//     id: 2,
+//     photo: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/1950s-laika-space-dog-russian-matchbox-label-retro-graphics.jpg",
+//     followed: false,
+//     fullName: "Mustag",
+//     status: "Im DOG!",
+//     adress: { country: "France", city: "Paris" },
+//   },
+//   {
+//     id: 3,
+//     photo: "https://cs10.pikabu.ru/post_img/2019/02/03/6/1549186262135686997.jpg",
+//     followed: true,
+//     fullName: "Tom",
+//     status: "Im pitia!",
+//     adress: { country: "France", city: "Paris" },
+//   },
+// ]
