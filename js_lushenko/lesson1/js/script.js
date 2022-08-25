e4a56045542a1924871195bbaeb83804c944689e
Павел <tepek92@mail.ru>
2022-08-25 14:19:21 +0700

    lesson 1

---
+++ js_lushenko/lesson1/js/script.js
@@ -0,0 +1,9 @@
+console.log("Pasha");
+console.log(11);
+
+
+document.querySelector(".two").innerHTML = 1992;
+document.getElementById("opss").innerHTML = "Hello";
+document.querySelector('#opss').innerHTML = "<b>BYBYB</b>";
+
+// console.log(a);
