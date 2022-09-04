af682522c5f1b6ff32d6096aabfd66e638124253
Павел <tepek92@mail.ru>
2022-09-04 16:23:30 +0700

    js training

--- training_js_kata/training.js
+++ training_js_kata/training.js
@@ -7,8 +7,36 @@ function helloWorld() {
   console.log(str);
 }
 // #2: Basic data types--Number
-
+var v1 = 50;
+(v2 = 100), (v3 = 150), (v4 = 200), (v5 = 2), (v6 = 250);
+function equal1() {
+  var a = v1;
+  var b = v1;
+  return a + b;
+}
+//Please refer to the example above to complete the following functions
+function equal2() {
+  var a = v3; //set number value to a
+  var b = v1; //set number value to b
+  return a - b;
+}
+function equal3() {
+  var a = v1; //set number value to a
+  var b = v5; //set number value to b
+  return a * b;
+}
+function equal4() {
+  var a = v4; //set number value to a
+  var b = v5; //set number value to b
+  return a / b;
+}
+function equal5() {
+  var a = v6; //set number value to a
+  var b = v3; //set number value to b
+  return a % b;
+}
 // #3: Basic data types--String
+
 // #4: Basic data types--Array
 // #5: Basic data types--Object
 // #6: Basic data types--Boolean and conditional statements if..else
