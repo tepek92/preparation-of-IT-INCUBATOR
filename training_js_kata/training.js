4092ff19935d35dbd638fc5bf3c860f778e71d4c
Павел <tepek92@mail.ru>
2022-09-04 17:01:36 +0700

    js training

--- training_js_kata/training.js
+++ training_js_kata/training.js
@@ -147,7 +147,15 @@ function padIt(str, n) {
   return str;
 }
 // #10: loop statement --for
-
+function pickIt(arr) {
+  var odd = [],
+    even = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) even.push(arr[i]);
+    else odd.push(arr[i]);
+  }
+  return [odd, even];
+}
 // #11: loop statement --break,continue
 // #12: loop statement --for..in and for..of
 // #13: Number object and its properties
